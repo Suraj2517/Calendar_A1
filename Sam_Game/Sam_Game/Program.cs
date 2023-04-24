23e776d83c5ef53b2934e931b5f2867bc6632610
Singh <suraj.singh4@dxc.com>
2023-04-24 16:58:23 +0530

    Updated

---
+++ Sam_Game/Sam_Game/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter No. of Robbers: ");
+int num = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter The Robber with whom Sam will start shooting: ");
+int start = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter the gap between the robbers: ");
+int gap = Convert.ToInt16(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    for (int j = start; j <= num; j += gap)
+    {
+        Console.WriteLine($"Target = {j}, {i} Shots Completed");
+    }
+    Console.WriteLine();
+}
